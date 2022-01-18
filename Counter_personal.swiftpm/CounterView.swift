4f02e7b3f8ec596189bb597823a26b36f633c048
Scope.h <hongsch07@naver.com>
2022-01-18 09:26:03 +0900

    [UPDATED]
    Created Actions for buttons and completed

--- Counter_personal.swiftpm/CounterView.swift
+++ Counter_personal.swiftpm/CounterView.swift
@@ -1,19 +1,34 @@
 import SwiftUI
 
 struct CounterView: View {
+    @State var count = 0
     var body: some View {
-        Button(action: {}) {
-            Text("0")
+        Button(action: {
+            count = increaseCount(count)
+        }) {
+            Text("\(count)")
                 .font(.system(size: 85))
                 .fontWeight(.bold)
                 .frame(maxWidth: .infinity, maxHeight: .infinity)
         }.padding()
-        Button(action: {}) {
-            Text("Clear")
+        Button(action: {
+            count = resetToZero(count)
+        }) {
+            Text("Reset")
                 .padding()
                 .frame(maxWidth: .infinity)
         }
             .buttonStyle(.bordered)
             .padding()
+    }
+    
+    func increaseCount(_ number: Int) -> Int {
+        var count = number
+        count += 1
+        return count
+    }
+    
+    func resetToZero(_ number: Int) -> Int {
+        return 0
     }
 }
